93e16971db9f3eb9308473ef5062fcc6523a091c
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-13 16:53:01 -0400

    user controller & model

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import SignUpUserValidator from 'App/Validators/SignUpUserValidator';
 import LogInUserValidator from 'App/Validators/LogInUserValidator';
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator';
 import User from 'App/Models/User';
 
 export default class UsersController {
+  public async index() {
+    return (await User.all()).map((user) => user.toJSON());
+  }
+
+  public async show({ params }: HttpContextContract) {
+    return (await User.findOrFail(params.id)).toJSON();
+  }
+
   public async signUp({ auth, request }: HttpContextContract) {
     const { email, password, name } = await request.validate(SignUpUserValidator);
     const user = await User.findBy('email', email);
@@ -11,8 +20,15 @@ export default class UsersController {
     return await auth.use('api').attempt(email, password);
   }
 
-  public async login({ auth, request}: HttpContextContract) {
+  public async login({ auth, request }: HttpContextContract) {
     const { email, password } = await request.validate(LogInUserValidator);
     return await auth.use('api').attempt(email, password);
+  }
+
+  public async update({ auth, request }: HttpContextContract) {
+    const { name } = await request.validate(UpdateUserValidator);
+    auth.user!.merge({ name });
+    await auth.user!.save();
+    return auth.user!.toJSON();
   }
 }
