a0ff6594fd05df9fe8c3f66ec65e92037a909044
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-13 17:02:40 -0400

    register auth middleware

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -1,14 +1,5 @@
-import { GuardsList } from '@ioc:Adonis/Addons/Auth';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { AuthenticationException } from '@adonisjs/auth/build/standalone';
 
-/**
- * Auth middleware is meant to restrict un-authenticated access to a given route
- * or a group of routes.
- *
- * You must register this middleware inside `start/kernel.ts` file under the list
- * of named middleware.
- */
 export default class AuthMiddleware {
   private validateUserPath({ auth, request, params }: HttpContextContract) {
     const requestArray = request.url().split('/');
