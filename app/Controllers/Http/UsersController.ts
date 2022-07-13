1a2946b3ad5acdef3615fd19c49cd31be8af7275
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-13 16:38:17 -0400

    user signup and login

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,18 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import SignUpUserValidator from 'App/Validators/SignUpUserValidator';
+import LogInUserValidator from 'App/Validators/LogInUserValidator';
+import User from 'App/Models/User';
 
-export default class UsersController {}
+export default class UsersController {
+  public async signUp({ auth, request }: HttpContextContract) {
+    const { email, password, name } = await request.validate(SignUpUserValidator);
+    const user = await User.findBy('email', email);
+    if (!user) await User.create({ email, password, name });
+    return await auth.use('api').attempt(email, password);
+  }
+
+  public async login({ auth, request}: HttpContextContract) {
+    const { email, password } = await request.validate(LogInUserValidator);
+    return await auth.use('api').attempt(email, password);
+  }
+}
